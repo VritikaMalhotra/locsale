7329699ba15b6429581cfa90ff6ff5e79c9197b9
VritikaMalhotra <vritikamalhotra23@gmail.com>
2021-05-08 00:49:32 +0530

    Edit profile backend left

--- app/src/main/java/com/example/locsaleapplication/EditProfileActivity.java
+++ app/src/main/java/com/example/locsaleapplication/EditProfileActivity.java
@@ -3,8 +3,18 @@ package com.example.locsaleapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    private ImageView close;
+    private CircleImageView imageprofile;
+    private TextView save;
+    private TextView changePhoto;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
