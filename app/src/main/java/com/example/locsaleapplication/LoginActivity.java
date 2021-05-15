1dc4695900c3e4726d8aa6cab30c579a10251d64
VritikaMalhotra <vritikamalhotra23@gmail.com>
2021-05-15 16:14:40 +0530

    Login bug fixed

--- app/src/main/java/com/example/locsaleapplication/LoginActivity.java
+++ app/src/main/java/com/example/locsaleapplication/LoginActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +19,13 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -28,6 +37,8 @@ public class LoginActivity extends AppCompatActivity {
     private TextView login_forgotPassword;
 
     private String email,password;
+
+    private String type;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -90,10 +101,36 @@ public class LoginActivity extends AppCompatActivity {
 
                 if(task.isSuccessful())
                 {
-                    finish();
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(intent);
+                    FirebaseDatabase.getInstance().getReference().child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                            .addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                    if(dataSnapshot.getValue() != null){
+                                        Map<StringBuffer, String> map = (Map) dataSnapshot.getValue();
+                                        if(map.get("type").equals("2")){
+                                            finish();
+                                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                            startActivity(intent);
+
+                                        }else{
+                                            Toast.makeText(LoginActivity.this, "This ID is associated with your Shopkeeper account", Toast.LENGTH_LONG).show();
+                                            Toast.makeText(LoginActivity.this, "Please register with another email for Customer side", Toast.LENGTH_LONG).show();
+                                            FirebaseAuth.getInstance().signOut();
+                                            startActivity(new Intent(LoginActivity.this,LoginActivity.class)
+                                                    .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                                            finish();
+                                        }
+
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                }
+                            });
+
                 }
                 else
                 {
@@ -104,6 +141,7 @@ public class LoginActivity extends AppCompatActivity {
         });
 
     }
+
 
     protected void onStart() {
         super.onStart();
