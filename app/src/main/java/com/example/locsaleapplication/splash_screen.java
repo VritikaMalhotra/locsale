dfda376fac68bd7849dfe31670cc50dba4daaaaf
VritikaMalhotra <vritikamalhotra23@gmail.com>
2021-06-25 13:46:42 +0530

    Splash screen backend done

--- app/src/main/java/com/example/locsaleapplication/splash_screen.java
+++ app/src/main/java/com/example/locsaleapplication/splash_screen.java
@@ -1,22 +1,32 @@
 package com.example.locsaleapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.lang.reflect.AnnotatedElement;
+import java.util.Map;
 
 public class splash_screen extends AppCompatActivity {
 
-   private ImageView splash_img;
-    private TextView splach_text;
-    private TextView splach_text_1;
+    private ImageView splash_img;
     private RelativeLayout relativeLayout;
 
     @Override
@@ -24,15 +34,13 @@ public class splash_screen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        splach_text = findViewById(R.id.splach_text);
         splash_img = findViewById(R.id.icon_image);
-        splach_text_1 = findViewById(R.id.splach_text_2);
         relativeLayout = findViewById(R.id.relativeLayout);
 
         relativeLayout.animate().alpha(0f).setDuration(1);
 
         TranslateAnimation animation = new TranslateAnimation(0,0,0,-1000);
-        animation.setDuration(1000);
+        animation.setDuration(2000);
         animation.setFillAfter(false);
         animation.setAnimationListener(new MyAnimationListener());
 
@@ -50,8 +58,17 @@ public class splash_screen extends AppCompatActivity {
         public void onAnimationEnd(Animation animation) {
             splash_img.clearAnimation();
             splash_img.setVisibility(View.INVISIBLE);
-            relativeLayout.animate().alpha(1f).setDuration(1000);
+            relativeLayout.animate().alpha(1f).setDuration(2000);
 
+            final Handler handler2 = new Handler(Looper.getMainLooper());
+            handler2.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    finish();
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                }
+            }, 2000);
 
         }
 
